e8cf6d1d607a5e0b53e9a925fb9a08dfe2c6ae53
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-09 13:12:08 -0800

    Update cols.m

--- HW6/cols.m
+++ HW6/cols.m
@@ -1,6 +1,6 @@
 function [f]=cols(v)
+
 % program: cols.m 
-% date:  6/20/96
 
 % returns the number of cols of a matrix
 % also see rows.m
@@ -8,7 +8,3 @@ function [f]=cols(v)
 
   dim = size(v);
   f = dim(2);
-
-
-
-
